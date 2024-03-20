78101d7b7a8c971e350041ae4c47f9be0ed0a6bb
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-03-20 12:51:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const parseCode = (str) => {
   // your code here
+	    let parts = str.split('0');
+
+    // Extract first name, last name, and id
+    let firstName = parts[0];
+    let lastName = parts[1];
+    let id = parts[2];
+
+    // Return an object with extracted properties
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        id: id
+    };
+	
 };
 
 // Do not change the code below
